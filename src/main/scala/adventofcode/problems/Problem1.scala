5f69bee8bbe04f3e43c2504e6698f93557e669bf
Maik de Kruif <maik@maik.dev>
2022-12-01 09:41:37 +0100

    Add Day 1

--- src/main/scala/adventofcode/problems/Problem1.scala
+++ src/main/scala/adventofcode/problems/Problem1.scala
@@ -4,10 +4,30 @@ import adventofcode.Problem
 
 object Problem1 extends Problem {
   override def part1(input: List[String]): String = {
-    0.toString
+    input
+      .mkString("\n")
+      .split("\n\n")
+      .map(_
+        .split('\n')
+        .map(_.toInt)
+        .sum
+      )
+      .max
+      .toString
   }
 
   override def part2(input: List[String]): String = {
-    0.toString
+    input
+      .mkString("\n")
+      .split("\n\n")
+      .map(_
+        .split('\n')
+        .map(_.toInt)
+        .sum
+      )
+      .sorted
+      .takeRight(3)
+      .sum
+      .toString
   }
 }
--- src/test/scala/adventofcode/problems/Problem1Spec.scala
+++ src/test/scala/adventofcode/problems/Problem1Spec.scala
@@ -7,21 +7,21 @@ import org.scalatest.matchers.should.Matchers
 class Problem1Spec extends AnyWordSpec with Matchers with TestUtils {
   "part 1" should {
     "solve the sample input" in {
-      Problem1.part1(getInput(1, "sample.txt")) shouldBe ""
+      Problem1.part1(getInput(1, "sample.txt")) shouldBe "24000"
     }
 
     "solve the actual input" in {
-      Problem1.part1(getInput(1, "input.txt")) shouldBe ""
+      Problem1.part1(getInput(1, "input.txt")) shouldBe "69912"
     }
   }
 
   "part 2" should {
     "solve the sample input" in {
-      Problem1.part2(getInput(1, "sample.txt")) shouldBe ""
+      Problem1.part2(getInput(1, "sample.txt")) shouldBe "45000"
     }
 
     "solve the actual input" in {
-      Problem1.part2(getInput(1, "input.txt")) shouldBe ""
+      Problem1.part2(getInput(1, "input.txt")) shouldBe "208180"
     }
   }
 }
